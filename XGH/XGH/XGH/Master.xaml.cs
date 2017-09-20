28b47d0fc15540b60c45db0dd451074db0b7b73c
Gustavo <Amaralgustavo59@gmail.com>
2017-09-19 21:02:21 -0300

    19.09.2017

---
+++ XGH/XGH/XGH/Master.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XGH
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : ContentPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+
+            BtnA.Clicked += async (sender, e) =>
+            {
+                await App.MasterDetail.Detail.Navigation.PushAsync(new P1());
+                App.MasterDetail.IsPresented = false;
+            };
+
+            BtnB.Clicked += async (sender, e) =>
+            {
+                await App.MasterDetail.Detail.Navigation.PushAsync(new P2());
+                App.MasterDetail.IsPresented = false;
+
+            };
+
+            BtnC.Clicked += async (sender, e) =>
+            {
+                await App.MasterDetail.Detail.Navigation.PushAsync(new P3());
+                App.MasterDetail.IsPresented = false;
+            };
+        }
+	}
+}
